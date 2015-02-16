5cfe5dc7c0730fe2b59550b8976556bcd0881ded
Deaboy <danieleandrus@gmail.com>
2015-02-16 11:57:02 -0700

    Moved functional commands to separate branch

---
+++ commands.h
@@ -0,0 +1,4 @@
+int onCommandCmdnm(const vector<string>& args);
+int onCommandPid(const vector<string>& args);
+int onCommandSystat(const vector<string>& args);
+int onDragonborn(const vector<string>& args);
